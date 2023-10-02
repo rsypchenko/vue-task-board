4bc744acd2ea788401b35491c603b6419f20efc5
imhotep <r.sipchenko@gmail.com>
2023-10-01 22:02:19 -0600

    Main component created with base grid

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,11 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-
-    
+      <v-row>
+        <v-col></v-col>
+        <v-col></v-col>
+        <v-col></v-col>
+      </v-row>
     </v-responsive>
   </v-container>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <HelloWorld />
+  <Main />
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import Main from '@/components/Main.vue'
 </script>
