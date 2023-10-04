b3bd6dcd2041e54d613eb9354ab5c6c7c8bc5eb9
imhotep <r.sipchenko@gmail.com>
2023-10-03 22:58:41 -0600

    small update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,14 +3,45 @@
     <v-responsive class="text-center fill-height">
       <v-row>
         <v-col>
-          <v-card v-for="todo in todos" :key="todo.id">
+          <v-card
+            variant="outlined"
+            color="indigo"
+            v-for="todo in todos"
+            :key="todo.id"
+          >
             <v-card-title>{{ todo.title }}</v-card-title>
-            <v-card-subtitle>{{ todo.subtitle }}</v-card-subtitle>
-            <v-card-text>{{ todo.text }}</v-card-text>
           </v-card>
         </v-col>
-        <v-col> </v-col>
-        <v-col> </v-col>
+        <v-col>
+          <v-card
+            variant="outlined"
+            color="indigo"
+            v-for="todo in progress"
+            :key="todo.id"
+          >
+            <v-card-title>{{ todo.title }}</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            variant="outlined"
+            color="indigo"
+            v-for="todo in review"
+            :key="todo.id"
+          >
+            <v-card-title>{{ todo.title }}</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            variant="outlined"
+            color="indigo"
+            v-for="todo in done"
+            :key="todo.id"
+          >
+            <v-card-title>{{ todo.title }}</v-card-title>
+          </v-card>
+        </v-col>
       </v-row>
     </v-responsive>
   </v-container>
@@ -19,5 +50,5 @@
 <script lang="ts" setup>
 import { useAppStore } from "@/store/app";
 
-const { todos } = useAppStore();
+const { todos, progress, review, done } = useAppStore();
 </script>
--- src/store/app.ts
+++ src/store/app.ts
@@ -5,9 +5,26 @@ const tasks = [
   {
     id: 1,
     title: "Task 1",
-    subtitle: "Subtitle 1",
-    text: "Text 1",
+    description: "Text 1",
     state: "todo",
+  },
+  {
+    id: 1,
+    title: "Task 2",
+    description: "Text 2",
+    state: "progress",
+  },
+  {
+    id: 1,
+    title: "Task 3",
+    description: "Text 3",
+    state: "review",
+  },
+  {
+    id: 1,
+    title: "Task 4",
+    description: "Text 4",
+    state: "done",
   },
 ];
 
@@ -19,5 +36,14 @@ export const useAppStore = defineStore("tasks", {
     todos(state) {
       return state.tasks.filter((task) => task.state === "todo");
     },
+    progress(state) {
+      return state.tasks.filter((task) => task.state === "progress");
+    },
+    review(state) {
+      return state.tasks.filter((task) => task.state === "review");
+    },
+    done(state) {
+      return state.tasks.filter((task) => task.state === "done");
+    }
   },
 });
